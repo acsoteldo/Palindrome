b4f50c831d7fab6dd7c1c56e55f64364894e4db0
Andrea Soteldo <76544489+acsoteldo@users.noreply.github.com>
2021-12-21 17:18:21 -0600

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -3,44 +3,61 @@
 //Project3
 //18Nov2020
 //This program should tell you whether a word is a palindrome or not.
-package src;
+package college;
 import java.util.Scanner;
 public class Palindrome 
 {	
-		public boolean isPalindrome(char[] a, int used)
-		{
-			a = new char[80];
-			String str = "";
-			used = str.length();
-			int i = 0, j = used - 1;
-			
-			while ((i < j) && ((str.charAt(i) != '.') || (str.charAt(i) != ' ')))
-			{
-				if (str.charAt(i) != str.charAt(j))
-					return false;
-					i++;
-					j--;
-			}
-			return true;
-		}
+	public static void main(String[] args)
+	{
+		char[] a = new char[80];
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter a text to check if it is a palindrome, end with period: ");
+		String str = keyboard.nextLine();
+		str = str.toUpperCase();
+		int used = str.length();
+		int i = 0; 
 		
-		public static void main(String[] args) 
+		while (i < str.length() && i < a.length) 
 		{
-			char[] a = new char[80];
-			Scanner keyboard = new Scanner(System.in);
-			System.out.print("Enter a text to check if it is a palindrome, end with period: ");
-			String str = "";
-			str = str.toUpperCase();
-			str = keyboard.nextLine();
-			int used = str.length();
+			a[i] = str.charAt(i);
+			i++;
+		}
 			
-			if (a.isPalindrome(a, used))
+		i = 0;
+			
+		while ((str.charAt(i) != '.') && (i != str.length() - 1))
+		{
+			if (isPalindrome(a, used))
 				System.out.println(str + " is a palindrome."); 
 			else
-				System.out.println(str + " is not a palindrome."); 
+				System.out.println(str + " is not a palindrome.");
+			break;
 		}
+	}
+	
+	public static boolean isPalindrome(char[] a, int used)
+	{
+		
+		int i = 0;		
+		int j = used - 1; 
+		
+		while (i < used/2)
 		{
-			System.out.println("Press 0 to quit, 1 to continue");
-			System.exit(0);
+			if (a[i] == ' ' || a[i] == '.') 
+			{
+				i++;
 			}
+			if (a[j] == ' ' || a[j] == '.') 
+			{
+				j--;
+			}
+			if (a[i] != a[j]) 
+			{
+				return false; 
+			}
+			i++;
+			j--;
+		}
+		return true;
+	}
 }
